f3050399957de2a5db4dc083dae5e38cfaf37bb6
lokeshsugandham <139068265+lokeshsugandham@users.noreply.github.com>
2026-08-09 11:13:05 +0530

    Delete Start-Local-Server.bat

--- Start-Local-Server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title YCC Charitable Trust - Local Web Server
-echo Starting YCC Charitable Trust Website on http://localhost:3000/ ...
-powershell -ExecutionPolicy Bypass -File "%~dp0server.ps1"
-pause
